220bbe08f2f44462d2959679b4e6d2e884237ab3
moamin8 <moamin-997@hotmail.com>
2023-05-24 20:10:34 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+
+const App = ()=> {
+  return (
+    
+<View style={styles.appContainer}>
+
+<Text>my first app</Text>
+
+
+</View>
+
+  )
+}
+
+export default App ; 
+
+const styles= StyleSheet.create({
+
+  appContainer:{
+  flex:1,
+  },
+ 
+})
+  
